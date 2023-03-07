57dc8681741b8a36d621fab5705a50a3c95e9fe4
reparazdiego <114122815+reparazdiego@users.noreply.github.com>
2023-03-07 16:22:14 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,15 @@
+
+function mostraruno() {
+    document.getElementById("textouno").innerHTML = "Maestro de escuela primaria con excelentes aptitudes demostradas con años de experiencia. Motivado y entusiasta para generar una gran atmósfera de aprendizaje.";}
+
+function mostrardos() {
+    document.getElementById("textodos").innerHTML = "Maestro de 3° grado - Primaria de Puerto Vallarta • 2014 hasta la fecha / Maestro de 1° grado - Escuela Primaria Audora • 2011 - 2014";} 
+
+function mostrartres() {
+    document.getElementById("textotres").innerHTML = "Universidad de Lago Azul - Licenciatura en Educación Primaria, 2012 / Academia de San Fernando - Mención honorífica de la generación 2008";}
+
+function mostrarcuatro() {
+    document.getElementById("textocuatro").innerHTML = "Planificación innovadora de clases / Organización de clases / Gestión de estilo de aprendizaje / Evaluaciones de rendimiento / Desarrollo de planes de estudio creativos";}
+
+function mostrarcinco() {
+    document.getElementById("textocinco").innerHTML = "Casa: 123-456-78990 / Celular: 123-456-77890 / flaruso@gmail.com / Av. Madero 123, CABA";}      
